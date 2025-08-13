fe2cb6197d85be948d9aa2e9827053ec9044f37c
shefket-mustafa <miracle_1@abv.bg>
2025-08-14 00:02:00 +0300

    Services translated

--- src/components/ServicesItem.tsx
+++ src/components/ServicesItem.tsx
@@ -1,5 +1,6 @@
 import Checkbox from '@mui/material/Checkbox';
 import { Link } from 'react-router';
+import { useTranslation } from "react-i18next";
 
 interface ServicesItemInterface {
   mvs: string;
@@ -20,6 +21,9 @@ export default function ServicesItem({
   checked,
   onSelect,
 }: ServicesItemInterface) {
+
+  const { t } = useTranslation("common");
+
   return (
     <div className={`bg-white rounded-2xl shadow-md ${checked ? 'border-2 border-blue-700' : ''} w-full md:w-[300px] lg:w-[350px] p-4 md:p-6 lg:p-8`}>
       <div className="flex flex-col space-y-3">
@@ -29,24 +33,24 @@ export default function ServicesItem({
             onChange={onSelect}
             inputProps={{ 'aria-label': 'controlled' }}
           />
-          <p>Check {mvs} MVs</p>
+          <p>{t("servicesItem.checkMvs", {count: Number(mvs) })}</p>
         </div>
 
         <div className="flex items-center gap-2">
-          <p className="text-2xl font-bold md:text-4xl">{pricePerReport}€</p>
-          <p className="text-lg"> / a report</p>
+          <p className="text-2xl font-bold md:text-4xl">{pricePerReport}</p>
+          <p className="text-lg">{t("servicesItem.perReport")}</p>
         </div>
 
         <div>
-          <p>You pay {yourPay}€.</p>
+          <p>{t("servicesItem.youPay", { amount: yourPay })}</p>
           <p className={`${mvs !== '1' ? 'text-red-600 line-through' : 'text-gray-400'}`}>
-            {mvs === '1' ? 'No discount' : noDiscountPay}
+            {mvs === '1' ? t("servicesItem.noDiscount") : noDiscountPay}
           </p>
         </div>
 
         <span className="self-start p-2 rounded-lg bg-amber-300 text-white">-{discount}%</span>
 
-        <Link className="text-sm underline" to="/refund-policy">Refund policy</Link>
+        <Link className="text-sm underline" to="/refund-policy">{t("servicesItem.refundPolicy")}</Link>
       </div>
     </div>
   );
